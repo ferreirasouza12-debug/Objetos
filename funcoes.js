4d881bf5494061ae0a2a72e0c10c2ce2dded6783
ferreirasouza12-debug <ferreira.souza12@escola.pr.gov.br>
2026-09-09 15:20:22 +0000

    repeticoes

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome: "João",
+    idade:23,
+    email:"Joao@firma.com",
+    telefone:["115555550", "114444440"],
+    saldo: 200,
+    efetuaPagamento: function (valor) {
+        if (valor > this.saldo) {
+            console.log("Saldo insuficiente");
+        } else {
+            this.saldo -= valor;
+                console.log(`Pagamento realizado. Novo saldo: ${this.saldo}`);
+        }
+    }
+};
+cliente.efetuaPagamento(250);
---
+++ repeticao.js
@@ -0,0 +1,19 @@
+const cliente = {
+nome: "Joao",
+idade: 24,
+email: "joao@firma.com",
+telefone: ["1155555550", "1144444440"],
+};
+
+cliente.endereços = [
+{
+rua: "r. Joseph Climber",
+numero: 1337,
+apartamento: true,
+complemento: "ap 934",
+}
+];
+
+for (let chave in cliente) {
+console.log(`A chave ${chave} tem o valor ${cliente[chave]}`);
+}
